b75835f90565b16276f6d4cda46968ae24a3f272
Federico Tagliabue <fede.tagliabue@gmail.com>
2017-04-03 20:54:12 +1000

    create genertor for feature.yml file

--- lib/generators/feature/install_generator.rb
+++ lib/generators/feature/install_generator.rb
@@ -1,7 +1,11 @@
+require 'rails/generators/base'
+
 module Feature
   class InstallGenerator < Rails::Generators::Base
+     source_root File.expand_path("../templates", __FILE__)
+
      def install
-        touch "/config/feature.yml"
+        copy_file "feature.example.yml", "./config/feature.yml"
      end
    end
 end
